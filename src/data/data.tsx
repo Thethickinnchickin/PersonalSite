bd01eb409b7bfc94fcb4dfe319e544bfd32b4ff2
Thethickinnchickin <matt.reiley@sjsu.edu>
2024-01-10 19:07:37 -0800

    a

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -17,7 +17,7 @@ import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-// import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
 // import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
 // import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
@@ -224,12 +224,12 @@ export const portfolioItems: PortfolioItem[] = [
     url: 'https://pnyx-orcin.vercel.app/',
     image: porfolioImage5,
   },
-  // {
-  //   title: 'Project title 6',
-  //   description: 'Give a short description of your project here.',
-  //   url: 'https://reactresume.com',
-  //   image: porfolioImage6,
-  // },
+  {
+    title: 'Project title 6',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage6,
+  },
   // {
   //   title: 'Project title 7',
   //   description: 'Give a short description of your project here.',
