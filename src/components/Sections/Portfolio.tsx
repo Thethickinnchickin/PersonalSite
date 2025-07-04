0c87be1765b4cf2662a8970474a37de9897d9aee
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-07-04 12:56:03 -0700

    styling updated

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -16,7 +16,7 @@ const Portfolio: FC = memo(() => {
             {/* Video Background */}
       <video
         autoPlay
-        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10 brackets"
         loop
         muted
         playsInline
