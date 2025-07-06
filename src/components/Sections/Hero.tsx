81635d7f9d17f25948bc76b0423f6181408a1cb4
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-07-06 12:02:16 -0700

    update styles

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: FC = memo(() => {
     <Section noPadding sectionId={SectionId.Hero}>
       <div className="moving-background flex h-screen w-full items-center justify-center">
         <div className="z-10 max-w-screen-lg px-4 lg:px-0">
-          <div className="flex flex-col card container-hero items-center gap-y-6 rounded-xl bg-gray-800 p-6 text-center shadow-lg backdrop-blur-sm">
+          <div className="flex flex-col card container-hero items-center gap-y-6 rounded-xl bg-gray-800 p-6 pt-10 text-center shadow-lg backdrop-blur-sm">
             <h1 className="text-4xl font-bold text-dark sm:text-5xl lg:text-7xl">{name}</h1>
             {description}
             <div className="flex gap-x-4 text-neutral-100 readable-text">
