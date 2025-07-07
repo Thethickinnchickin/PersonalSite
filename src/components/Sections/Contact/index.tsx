5442d4e49fa11e590815b6896eba8d5377bff21c
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-07-06 23:00:30 -0700

    test

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -27,7 +27,7 @@ const Contact: FC = memo(() => {
   const {headerText, description, items} = contact;
   return (
     <Section className="container-check readable-text" sectionId={SectionId.Contact}>
-      <div className='container-hero p-5'>
+      <div className='email-border p-5'>
         <div className="flex flex-col gap-y-6 md:flex-row md:items-center z-10">
           <EnvelopeIcon className="hidden h-16 w-16 text-white md:block" />
           <h2 className="text-2xl font-bold text-white">{headerText}</h2>
