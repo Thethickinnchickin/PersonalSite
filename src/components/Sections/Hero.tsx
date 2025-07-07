a43ddec272fe8b51dcfce682aecf4ed292cbbd0a
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-07-06 19:23:11 -0700

    testing

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: FC = memo(() => {
     <Section noPadding sectionId={SectionId.Hero}>
       <div className="moving-background flex h-screen w-full items-start justify-center pt-20">
 
-        <div className="z-10 max-w-screen-lg px-4 lg:px-6">
+        <div className="z-10 max-w-screen-lg px-2 lg:px-6">
           <div className="flex flex-col card container-hero items-center gap-y-6 rounded-xl bg-gray-800 p-6 pt-10 text-center shadow-lg backdrop-blur-sm">
             <h1 className="text-4xl font-bold text-dark sm:text-5xl lg:text-7xl">{name}</h1>
             {description}
