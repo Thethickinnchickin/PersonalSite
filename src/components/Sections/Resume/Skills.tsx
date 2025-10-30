77dd8813d1e9cff0b89922055e2210457bddecbd
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-10-30 11:45:05 -0700

    editting

--- src/components/Sections/Resume/Skills.tsx
+++ src/components/Sections/Resume/Skills.tsx
@@ -5,7 +5,7 @@ import {Skill as SkillType, SkillGroup as SkillGroupType} from '../../../data/da
 export const SkillGroup: FC<PropsWithChildren<{skillGroup: SkillGroupType}>> = memo(({skillGroup}) => {
   const {name, skills} = skillGroup;
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col text-white">
       <span className="text-center text-lg font-bold">{name}</span>
       <div className="flex flex-col gap-y-2">
         {skills.map((skill, index) => (
@@ -23,7 +23,7 @@ export const Skill: FC<{skill: SkillType}> = memo(({skill}) => {
   const percentage = useMemo(() => Math.round((level / max) * 100), [level, max]);
 
   return (
-    <div className="readable-text flex flex-col">
+    <div className="readable-text flex flex-col text-white">
       <span className="ml-2 text-sm font-medium">{name}</span>
       <div className="h-5 w-full overflow-hidden rounded-full bg-neutral-300">
         <div className="h-full rounded-full bg-orange-400" style={{width: `${percentage}%`}} />
